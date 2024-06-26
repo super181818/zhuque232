c1adf19d374013e83560048a379ea8397165d665
super181818 <131360198+super181818@users.noreply.github.com>
2024-06-26 14:26:46 +0800

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,10 @@
+/*-------config start-------*/
+var _wip = '43.136.75.232';  
+var _port = '1889';   
+/*---------config end---------*/
+
+var BaseUrl = 'http://' + _wip+':'+_port;
+var ResourceUrl = '/';
+var Suffix = '.html';
+var _isTx = 1;
+var _wport = '20000'
